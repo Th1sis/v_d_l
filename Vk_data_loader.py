8f3578a40b48d16d500da2b8e075d818a245546f
Th1sis <33522515+Th1sis@users.noreply.github.com>
2017-11-11 15:17:38 +0200

    new_errors_review

--- Vk_data_loader.py
+++ Vk_data_loader.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[148]:
+# In[156]:
 
 
 import json
@@ -27,17 +27,12 @@ def split_ids(list_id, sublist_vol):
     data_ids=[]
     n=sublist_vol
     vol=len(list_id)//n
-    pprint(vol)
-    pprint(list_id)
     for i in range(vol):
         if i==0:
             data_ids.append(list_id[0:n])
-            pprint(data_ids)
         else:
             data_ids.append(list_id[i*n:(i+1)*n])
-            pprint(data_ids)
     data_ids.append(list_id[vol*n:])
-    pprint(data_ids)
     return data_ids
 
 if __name__ == "__main__":
@@ -47,19 +42,42 @@ if __name__ == "__main__":
         print('слишком много параметров. Требуется указать название скрипта и путь к файлу входных данных формата .json')
     else:
         data=data_read(sys.argv[1])
-        data_ids=split_ids(data["ID"], 1000)
-
-    try:
-        session=vk.Session(access_token='b17628ecb17628ecb17628ec02b1294278bb176b17628eceb73d9a475ca00cb1e97dadb')
-        vk_api=vk.API(session)
-        c=0
-        for lists in data_ids:
-            res=vk_api.users.get(user_ids=lists, fields=data["fields"])
-            pprint(res)
-            c+=1
-            if c % 3 == 0:
-                time.sleep(1)
-    except OSError:
-            pprint('прервано интернет- соединение')
+        try:
+            data_ids=split_ids(data["ID"], 1000)
+        except KeyError:
+            print('отсутствует поле ID в исходном файле')
         
+
+        try:
+            session=vk.Session(access_token='b17628ecb17628ecb17628ec02b1294278bb176b17628eceb73d9a475ca00cb1e97dadb')
+            vk_api=vk.API(session)
+            c=0
+            requests_number_per_second=3
+            for lists in data_ids:
+                try:
+                    res=vk_api.users.get(user_ids=lists, fields=data["fields"])
+                    pprint(res)
+                    c+=1
+                    if c % requests_number_per_second == 0:
+                        time.sleep(1)
+                except KeyError:
+                    print('отсутствует поле fields в исходном файле')                
+        except KeyError:
+            pprint('отсутствует поле fields в исходном файле')
+            sys.exit() 
+        except ConnectionAbortedError:
+            pprint('сервис разорвал соединение')
+            sys.exit() 
+        except ConnectionRefusedError:
+            pprint('в попытке соединения отказано')
+            sys.exit() 
+        except BrokenPipeError:
+            pprint('ошибка соединения')
+            sys.exit() 
+        except ConnectionResetError:
+            pprint('соединение сброшено')
+            sys.exit() 
+        except OSError:
+            pprint('отсутствует интернет-соединение')
+            sys.exit()        
 
